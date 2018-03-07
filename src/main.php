9ab78e1aaed70e44c8edf20f819e98f47d77fd23
imendoza03 <ivanmendoza@gmail.com>
2018-03-07 12:05:16 +0100

    Changes for the main class

---
+++ src/main.php
@@ -0,0 +1,106 @@
+<?php
+
+use Tools\Metadata;
+use Tools\Serializer;
+use Tools\Normalizers;
+
+class Car 
+{
+    
+    private $plateNumber;
+    private $color;
+    private $brand;
+    private $constructionYear;
+    private $numberOfDoors;
+    
+    private $registeredCars = [];
+    
+    public function __construct(){
+        
+        $counter = 0;
+        
+        if(empty($this->registeredCars)){
+            $counter++;
+            $this->registeredCars[] = $counter;
+        }
+        
+        $this->registeredCars[] = $counter++;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getPlateNumber()
+    {
+        return $this->plateNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConstructionYear()
+    {
+        return $this->constructionYear;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumberOfDoors()
+    {
+        return $this->numberOfDoors;
+    }
+
+    /**
+     * @param mixed $color
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * @param mixed $brand
+     */
+    public function setBrand($brand)
+    {
+        $this->brand = $brand;
+    }
+
+    /**
+     * @param mixed $constructionYear
+     */
+    public function setConstructionYear($constructionYear)
+    {
+        $this->constructionYear = $constructionYear;
+    }
+
+    /**
+     * @param mixed $numberOfDoors
+     */
+    public function setNumberOfDoors($numberOfDoors)
+    {
+        $this->numberOfDoors = $numberOfDoors;
+    }
+}
+
+$car = new Car();
+
+$metadata = new Metadata();
+}
